9362f45f801a763ac632e1c6c3ae1dc9af698676
Yuri Hinostroza <sistmasyhf@gmail.com>
2026-03-16 16:22:54 -0500

    modificaciones ordenamiento

---
+++ CPP/estructuraDatos/AlgoritmosDeOrdenamiento/uno.cpp
@@ -0,0 +1,12 @@
+// Dao un arreglo de número enteros, ordénalo de menor a mayor utilizando el método burbuja o selección
+// int arreglo = {3,7,1,2}
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout  << "Ingrese el tamaño del arreglo: ";
+
+    return 0;
+}
